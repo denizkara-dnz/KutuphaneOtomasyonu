ce60e3da5d4d2e6bad459266faef171f74358e65
denizkara-dnz <denizkara_dnz@hotmail.com>
2020-08-10 16:05:38 +0300

    Form ismi ve başlığı değiştirildi

--- KutuphaneOtomasyonu/Form1.cs
+++ KutuphaneOtomasyonu/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace KutuphaneOtomasyonu
 {
-    public partial class Form1 : Form
+    public partial class frmAnaForm : Form
     {
-        public Form1()
+        public frmAnaForm()
         {
             InitializeComponent();
         }
